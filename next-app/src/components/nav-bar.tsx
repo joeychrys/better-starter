9dbbc6a390784e9f18a3d18e186e04f612b730e1
joeychrys <joeyachrys@outlook.com>
2025-04-17 00:24:12 -0500

    removed agents page

--- next-app/src/components/nav-bar.tsx
+++ next-app/src/components/nav-bar.tsx
@@ -36,9 +36,6 @@ export default async function NavBar() {
                     <Link href="/pricing" className="hover:underline underline-offset-4">
                         Pricing
                     </Link>
-                    <Link href="/agents" className="hover:underline underline-offset-4">
-                        Agents
-                    </Link>
                 </div>
                 <div className="flex items-center space-x-4">
                     {session ? (
