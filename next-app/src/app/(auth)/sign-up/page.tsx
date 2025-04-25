17d1c2b0836e6ae51b018dce6b4c0c7788a07d1c
joeychrys <joeyachrys@outlook.com>
2025-04-24 22:38:54 -0500

    updated refresh on signup

--- next-app/src/app/(auth)/sign-up/page.tsx
+++ next-app/src/app/(auth)/sign-up/page.tsx
@@ -62,6 +62,7 @@ export default function SignUpPage() {
                 },
                 onSuccess: async () => {
                     router.push("/")
+                    router.refresh()
                 }
             }
         })
