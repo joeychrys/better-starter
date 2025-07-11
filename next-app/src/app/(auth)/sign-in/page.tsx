37a1fd2398143e46ebac2f22d461333e5a651aa5
joeychrys <joeyachrys@outlook.com>
2025-07-10 22:59:58 -0500

    upgraded tailwind, shadcn, next to v15, and fixed ui changes caused by shadcn

--- next-app/src/app/(auth)/sign-in/page.tsx
+++ next-app/src/app/(auth)/sign-in/page.tsx
@@ -106,8 +106,8 @@ export default function SignInPage() {
               </Form>
 
               {/* Social Sign In */}
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">
+              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <span className="bg-card text-muted-foreground relative z-10 px-2">
                   Or continue with
                 </span>
               </div>
